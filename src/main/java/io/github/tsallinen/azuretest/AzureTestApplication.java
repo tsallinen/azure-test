a57511d60e821a4a1e1c44e88cd9dc8c9e58697b
Timo Sallinen <timo.sallinen@nls.fi>
2023-02-09 22:20:54 +0200

    Initial version.

---
+++ src/main/java/io/github/tsallinen/azuretest/AzureTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.tsallinen.azuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tsallinen/azuretest/AzureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tsallinen.azuretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
